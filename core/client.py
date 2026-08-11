9d16a9a73e95380210c5295319064837e8366069
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:21:16 +0530

    style: imports

--- core/client.py
+++ core/client.py
@@ -1,9 +1,8 @@
-import os,logging
 from pyrogram import Client
 from config.constants import APP_NAME
+import logging,os
 
 logger=logging.getLogger(__name__)
 
-def create_client(api_id,api_hash,bot_token=None):
- logger.info(f"Creating {APP_NAME} client...")
- return Client(APP_NAME,api_id=api_id,api_hash=api_hash,bot_token=bot_token)
+def make(api_id,api_hash,token=None):
+ return Client(APP_NAME,api_id=api_id,api_hash=api_hash,bot_token=token)
